a0fac0b1096bc7a8a3d05630f279224a0b1c1fa9
ququzone <xueping.yang@gmail.com>
2022-10-15 11:53:08 +0800

    add path to wallet new

--- cast-ext/commands/wallet/new.go
+++ cast-ext/commands/wallet/new.go
@@ -4,10 +4,15 @@ import (
 	"crypto/ecdsa"
 	"errors"
 	"fmt"
+	"os"
+	"syscall"
 
+	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/google/uuid"
 	"github.com/urfave/cli/v2"
+	"golang.org/x/term"
 )
 
 type New struct {
@@ -23,6 +28,28 @@ func (n *New) Command() *cli.Command {
 		Aliases: []string{"w"},
 		Usage:   "cast-ext wallet new [PATH]",
 		Action: func(ctx *cli.Context) error {
+			path := ctx.Args().First()
+			var password string
+
+			if path != "" {
+				fmt.Println("Enter password:")
+				pw, err := term.ReadPassword(int(syscall.Stdin))
+				if err != nil {
+					return errors.New("read password error")
+				}
+
+				fmt.Println("Re-enter password:")
+				pw1, err := term.ReadPassword(int(syscall.Stdin))
+				if err != nil {
+					return errors.New("read password error")
+				}
+
+				if string(pw) != string(pw1) {
+					return errors.New("password dismatch")
+				}
+				password = string(pw)
+			}
+
 			privateKey, err := crypto.GenerateKey()
 			if err != nil {
 				return errors.New("generate private key error")
@@ -34,13 +61,40 @@ func (n *New) Command() *cli.Command {
 				return errors.New("generate public key error")
 			}
 			publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
-			address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
-			fmt.Printf(
-				"Successfully created new keypair.:\nAddress: %s\nPrivate Key: %s\nPublic Key: %s\n",
-				address,
-				hexutil.Encode(privateKeyBytes)[2:],
-				hexutil.Encode(publicKeyBytes)[2:],
-			)
+			address := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+			if path == "" {
+				fmt.Printf(
+					"Successfully created new keypair.:\nAddress: %s\nPrivate Key: %s\nPublic Key: %s\n",
+					address.Hex(),
+					hexutil.Encode(privateKeyBytes)[2:],
+					hexutil.Encode(publicKeyBytes)[2:],
+				)
+			} else {
+				key := &keystore.Key{
+					Id:         uuid.New(),
+					Address:    address,
+					PrivateKey: privateKey,
+				}
+
+				data, err := keystore.EncryptKey(key, password, keystore.StandardScryptN, keystore.StandardScryptP)
+				if err != nil {
+					return errors.New("encrypt keystore error")
+				}
+				file, err := os.Create(path + address.Hex())
+				if err != nil {
+					return errors.New("open keystore file error")
+				}
+				if _, err = file.Write(data); err != nil {
+					return errors.New("write keystore file error")
+				}
+				fmt.Printf(
+					"Successfully created new keypair.:\nAddress: %s\nPublic Key: %s\nPath: %s\n",
+					address.Hex(),
+					hexutil.Encode(publicKeyBytes)[2:],
+					path+address.Hex(),
+				)
+			}
 			return nil
 		},
 	}
--- go.mod
+++ go.mod
@@ -4,15 +4,20 @@ go 1.19
 
 require (
 	github.com/ethereum/go-ethereum v1.10.25
+	github.com/google/uuid v1.2.0
 	github.com/urfave/cli/v2 v2.19.2
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
 )
 
 require (
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/rjeczalik/notify v0.9.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 )
